34f52e13f31afec06008e9587e7c1b516b2a6c6b
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-07-12 16:45:42 +0200

    collide ball with racket

--- src/js/physics/Collision.js
+++ src/js/physics/Collision.js
@@ -43,7 +43,23 @@ function collideBallWithRackets( ball ) {
 
 			plane.setFromNormalAndCoplanarPoint( vec3, racket.position )
 
-			console.log( plane.intersectsLine( line3 ) )
+			// if the racket cross the line created from ball current and last positions
+
+			if ( plane.intersectsLine( line3 ) ) {
+
+				vec3
+				.multiplyScalar( ball.velocity.length() )
+				.negate();
+
+				// move ball to position where it didn't yet cross the racket,
+				// in order to avoid a new collision on the way back,
+				// then apply the new velocity.
+				
+				ball.position.copy( ballsPos[ ball.id ] );
+
+				ball.velocity.copy( vec3 );
+
+			}
 
 		})
 
