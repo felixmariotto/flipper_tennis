f894a85103aa4804ee608e7bb2e709a00e457f7c
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-07-14 11:34:27 +0200

    add four planes to the scene

--- src/js/physics/World.js
+++ src/js/physics/World.js
@@ -1,4 +1,19 @@
 
-console.log('hello world')
+import * as THREE from 'three';
+import Scene from '../core/Scene.js';
+
+const planes = [
+	new THREE.Plane( new THREE.Vector3( -1, 0, 0 ), 2 ),
+	new THREE.Plane( new THREE.Vector3( 1, 0, 0 ), 2 ),
+	new THREE.Plane( new THREE.Vector3( 0, -1, 0 ), 2 ),
+	new THREE.Plane( new THREE.Vector3( 0, 1, 0 ), 2 )
+];
+
+planes.forEach( (plane) => {
+
+	var helper = new THREE.PlaneHelper( plane, 1, 0xffff00 );
+	Scene.threeScene.add( helper );
+
+});
 
 export default undefined
