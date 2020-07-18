0a38965ec1bd73ee3f2f207cf4cf6796e69e77ca
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-07-18 18:40:03 +0200

    create level tiles in the menu

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -11,6 +11,9 @@ const BIG_SCREEN_HEIGHT = 1.5;
 const BIG_SCREEN_WIDTH = 1.5;
 const BIG_SCREEN_DEPTH = 0.1;
 
+const LEVEL_TILE_HEIGHT = (BIG_SCREEN_HEIGHT - 0.28) / 5;
+const LEVEL_TILE_WIDTH = (BIG_SCREEN_WIDTH - 0.20) / 4;
+
 const BIG_SCREEN_POS = new THREE.Vector3( 0, 1.25, -1 );
 
 //
@@ -69,6 +72,63 @@ titleContainer.add(
 );
 
 uiContainer.add( titleContainer );
+
+// rows
+
+const rows = [];
+
+for ( let i=0 ; i<5 ; i++ ) {
+
+	const row = new ThreeMeshUI.Block({
+		height: LEVEL_TILE_HEIGHT + 0.017,
+		width: BIG_SCREEN_WIDTH,
+		backgroundColor: new THREE.Color( 0xff00ff ),
+		offset: 0,
+		backgroundOpacity: 0,
+		contentDirection: 'row',
+		justifyContent: 'center',
+		alignContent: 'center'
+	});
+
+	uiContainer.add( row );
+
+	rows.push( row );
+
+}
+
+// level tiles
+
+rows.forEach( (row) => {
+
+	const rowColor = new THREE.Color(
+		(Math.random() * 0.2) + 0.2,
+		(Math.random() * 0.2) + 0.2,
+		(Math.random() * 0.2) + 0.2
+	)
+
+	for ( let i=0 ; i<4 ; i++ ) {
+
+		const tile = new ThreeMeshUI.Block({
+			height: LEVEL_TILE_HEIGHT,
+			width: LEVEL_TILE_WIDTH,
+			margin: 0.01,
+			backgroundColor: rowColor,
+			justifyContent: 'center',
+			alignContent: 'center',
+			fontSize: 0.1
+		});
+
+		tile.add(
+			new ThreeMeshUI.Text({
+				content: String( i )
+			})
+		)
+
+		row.add( tile );
+
+	}
+
+})
 
 //
 
